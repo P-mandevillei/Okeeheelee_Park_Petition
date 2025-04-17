38d540d111ee8761ac2f239f89526f91d4f07a67
P-mandevillei <chenglin.l@wustl.edu>
2025-04-17 17:10:26 -0500

    refinement

--- client/public/src/components/auth/Cookies.jsx
+++
@@ -1,9 +0,0 @@
-export default function getCookie(name) {
-    // https://flask-jwt-extended.readthedocs.io/en/stable/token_locations.html
-    const cookies = `; ${document.cookie}`;
-    const target = cookies.split(`; ${name}=`);
-    if (target.length === 2) {
-        return target[1].split(";")[0]
-    }
-    return null;
-}
--- client/public/src/components/pages/PublicLanding.jsx
+++
@@ -1,7 +0,0 @@
-export default function PublicLanding() {
-    return <>
-        <p>
-            You have arrived at an uncharted territory!
-        </p>
-    </>
-}
