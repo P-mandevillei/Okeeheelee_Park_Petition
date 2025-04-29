97001b52c84d6d977f352029a6ae492b590fd956
P-mandevillei <chenglin.l@wustl.edu>
2025-04-28 20:14:19 -0500

    changed layout

--- client/public/src/components/pages/PublicHomepage.jsx
+++ client/public/src/components/pages/PublicHomepage.jsx
@@ -12,26 +12,26 @@ export default function PublicHomepage(props) {
 
     return <div style={{"paddingBottom": `${padBottomPx}px`}}>
         <Card style={{"border": "rgba(0,0,0,0)", 'position': 'relative'}}>
-            <img src={background_1} alt="background image" style={{'position': 'relative', 'zIndex': 1, "filter": "blur(8px)", "-webkit-filter": "blur(8px)"}}/> 
+            <img src={background_1} alt="background image" style={{'position': 'relative', 'zIndex': 1, "filter": "blur(8px)", "WebkitFilter": "blur(8px)"}}/> 
             <Container style={{'padding': '5vw', 'position':'absolute', 'zIndex': 999}}>
-                
-                <p className="center whiteBold" style={{fontSize: "6vw", 'padding': "1vw"}}>Protect Okeeheelee Park!</p>
-                <p className="center" style={{"color": "rgb(246, 240, 240)", fontSize: "3vw", 'padding': "6vw"}}>
+                <p className="center whiteBold" style={{fontSize: "6vw", 'padding': "2vw"}}>Protect Okeeheelee Park!</p>
+                <Col xs={12} className="center">
+                    <Button onClick={()=>{nav('/contribute')}} className="secondaryColor secondaryColorHover" style={{'fontSize': "2.5vw", 'width': '50%'}}>
+                        Contribute!
+                    </Button>
+                </Col>
+                <p className="center" style={{"color": "rgb(246, 240, 240)", fontSize: "3vw", 'padding': "3vw"}}>
                     Call for reallocation of <span className="notice">~$4.1M</span> funds <br/> 
                     toward <span style={{'fontWeight': "bold"}}>natural restoration</span> <br/> 
                     instead of turning it into an RV park 
                 </p>
                 <Row style={{'padding': '3vw'}}>
-                <Col xs={6} className="center">
+                <Col xs={12} className="center">
                     <span className="selectablePrimaryFlipped" onClick={()=>nav("/details")} style={{'fontSize': "2.5vw"}}>
                         Read More
                     </span>
                 </Col>
-                <Col xs={6} className="center">
-                    <Button onClick={()=>{nav('/contribute')}} className="primaryColor primaryHover" style={{'fontSize': "2.5vw"}}>
-                        Contribute!
-                    </Button>
-                </Col>
+                
                 </Row>
             </Container>
         </Card>
