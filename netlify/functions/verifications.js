5d197428dfb996d7bdaea6e17c1f260f9f4e0553
P-mandevillei <chenglin.l@wustl.edu>
2025-07-20 06:33:57 -0500

    google recaptcha

--- netlify/functions/verifications.js
+++ netlify/functions/verifications.js
@@ -17,16 +17,18 @@ function is_valid_US_phone(phone) {
 
 async function recaptcha_validate(token) {
     // reCAPTCHA: https://developers.google.com/recaptcha/docs/verify
+    /*
     const body = {
         'secret': GOOGLE_RECAPTCHA_SERVER_KEY,
         'response': token
     }
+    */
+    const params = new URLSearchParams();
+    params.append('secret', GOOGLE_RECAPTCHA_SERVER_KEY);
+    params.append('response', token);   
     const res = await fetch(GOOGLE_RECAPTCHA_API, {
-        headers: {
-            'content-type': 'application/json'
-        },
         method: 'POST',
-        body: JSON.stringify(body)
+        body: params
     });
     const result = await res.json();
     return result.success;
