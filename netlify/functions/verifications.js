13c41712d5adb98eb23a9eeed43ea2184d71e525
P-mandevillei <chenglin.l@wustl.edu>
2025-07-20 05:42:27 -0500

    fixed js import

--- netlify/functions/verifications.js
+++ netlify/functions/verifications.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 const GOOGLE_RECAPTCHA_API = process.env.GOOGLE_RECAPTCHA_API;
 const GOOGLE_RECAPTCHA_SERVER_KEY = process.env.GOOGLE_RECAPTCHA_SERVER_KEY;
